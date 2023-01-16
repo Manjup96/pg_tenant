8c21bdbb9da2172babb3eb4dc013cb2a38e31882
Bhargav983 <71189878+Bhargav983@users.noreply.github.com>
2023-01-16 18:27:46 +0530

    Update test.php
    bhargav

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <h1> for testing </h1>
 
-<<<<<<< HEAD
 <h2> By manju</h2>
-=======
 <h2> By Bhargav </h2>
->>>>>>> 271fe7982a99a5c3e46b7441c6f586619871d3a4
 
 
 <h3>Bhagath testing </h3>
+
+<h2> By Bhargav </h2>
